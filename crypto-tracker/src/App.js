f538cc0065ce61ecc813a1cd67afe17f8a5de2e9
Ed <edwardaacowell@gmail.com>
2020-12-29 17:03:45 +0000

    api func made with react hooks, app tags made with their class names

--- crypto-tracker/src/App.js
+++ crypto-tracker/src/App.js
@@ -1,10 +1,35 @@
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
+
 
 function App() {
+  const [coins, setCoins] = useState([])
+
+useEffect(() => {
+  axios.get(
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+  ).then(res => {
+   // console.log('response --->', res)
+    setCoins(res.data)
+  }).catch(err => console.log(err))
+}, [])
+
+
+
   return (
-    <div className="App">
+    <div className="crypto-tracker">
+      <div className="crypto-search">
+        <h1 className="crypto-title">Eds Crypto Tracker</h1>
+        <form>
+        <input type='text' placeholder='search here...' className='search-input'/>
+        </form>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+//https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
